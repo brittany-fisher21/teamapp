2efd908d411bc996cb9f1d1be49d0718ebdbe8b5
Brittany Fisher <bribreana7@gmail.com>
2022-01-26 23:09:57 -0500

    feat: click added to button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   newMemberName="";
   members: string [] =[];
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);//displays new member name
+  }
+
+  addMember(){
+    console.log('I CLICKED IT'); //confirmed the click!
+    this.members.push(this.newMemberName);
+    console.log(this.members);//displays empty string
+
+  }
 }
